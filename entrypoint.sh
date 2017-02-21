5b1885761ecefc2675d19beb61e2afbf8781ea44
Dirk Lüth <dlueth@Dirks-MBP.fritz.box>
2017-02-22 00:22:38 +0100

    remove output-type

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,34 @@
 #!/bin/sh
 
-OUTFILE_PATH="/tmp/out"
-OUTFILE_TYPE=""
-CONVERSION_PARAMS="-strip"
-
-alias echoerr='>&2 echo'
-
-if [  $# -le 1 ]
+if [ $# -le 1 ]
 then
 	exit 1
 fi
 
-if [ $# -ge 3 ]
-then
-	OUTFILE_TYPE=$3
-else
-	OUTFILE_TYPE="jpg"
-fi
+OUTFILE_PATH="/tmp/out"
+CONVERSION_PARAMS="-strip -quiet"
+INFILE_TYPE=$(su - imagemagick -c "identify -quiet -format '%m' $1")
+INFILE_PAGES=$(su - imagemagick -c "identify -quiet -format '%n' $1")
 
-case "$OUTFILE_TYPE" in
-	jpeg|jpg)
+case "$INFILE_TYPE" in
+	JPEG|JPG|PNG)
 		OUTFILE_PATH="${OUTFILE_PATH}.jpg"
-		CONVERSION_PARAMS="${CONVERSION_PARAMS} -resize 100% -interlace Plane -sampling-factor: 4:2:0 -type Grayscale -quality 85%"
+		CONVERSION_PARAMS="${CONVERSION_PARAMS} -resize 100% -interlace Plane -sampling-factor: 4:2:0 -type Grayscale -quality 75%"
 		;;
-	png)
-		OUTFILE_PATH="${OUTFILE_PATH}.png"
-		CONVERSION_PARAMS="${CONVERSION_PARAMS} -resize 100% -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -type Grayscale"
-		;;
-	pdf)
-		OUTFILE_PATH="${OUTFILE_PATH}.pdf"
-		CONVERSION_PARAMS="${CONVERSION_PARAMS} -density 200 -page A4 -compress Zip -type Grayscale -quality 85"
+	PBM|PDF|TIFF)
+		if [ "$INFILE_PAGES" -gt 1 ]
+		then
+			OUTFILE_PATH="${OUTFILE_PATH}.pdf"
+			CONVERSION_PARAMS="${CONVERSION_PARAMS} -density 200 -page A4 -compress Zip -type Grayscale -quality 75"
+		else
+			OUTFILE_PATH="${OUTFILE_PATH}.jpg"
+			CONVERSION_PARAMS="${CONVERSION_PARAMS} -density 200 -resize 100% -interlace Plane -sampling-factor: 4:2:0 -type Grayscale -quality 75%"
+		fi
 		;;
 	*)
 		exit 1
 esac
 
-touch $OUTFILE_PATH && chown imagemagick:imagemagick $OUTFILE_PATH
+touch ${OUTFILE_PATH} && chown imagemagick:imagemagick ${OUTFILE_PATH}
 su - imagemagick -c "convert ${CONVERSION_PARAMS} ${1} ${OUTFILE_PATH}"
-cat $OUTFILE_PATH > $2
+cat ${OUTFILE_PATH} > $2
