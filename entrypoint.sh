253fe451ce9d2904dc5dec6f35bb5c064cf9812d
Dirk Lüth <dlueth@Dirks-MBP.fritz.box>
2017-02-21 23:01:14 +0100

    tweak conversion parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 OUTFILE_PATH="/tmp/out"
 OUTFILE_TYPE=""
-CONVERSION_PARAMS="-resize 100%"
+CONVERSION_PARAMS="-strip"
 
 alias echoerr='>&2 echo'
 
@@ -21,18 +21,20 @@ fi
 case "$OUTFILE_TYPE" in
 	jpeg|jpg)
 		OUTFILE_PATH="${OUTFILE_PATH}.jpg"
+		CONVERSION_PARAMS="${CONVERSION_PARAMS} -resize 100% -interlace Plane -sampling-factor: 4:2:0 -type Grayscale -quality 85%"
 		;;
 	png)
 		OUTFILE_PATH="${OUTFILE_PATH}.png"
+		CONVERSION_PARAMS="${CONVERSION_PARAMS} -resize 100% -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -type Grayscale"
 		;;
 	pdf)
 		OUTFILE_PATH="${OUTFILE_PATH}.pdf"
-		CONVERSION_PARAMS="-density 150 -page A4"
+		CONVERSION_PARAMS="${CONVERSION_PARAMS} -density 200 -page A4 -compress Zip -type Grayscale -quality 85"
 		;;
 	*)
 		exit 1
 esac
 
 touch $OUTFILE_PATH && chown imagemagick:imagemagick $OUTFILE_PATH
-su - imagemagick -c "convert -strip ${CONVERSION_PARAMS} ${1} ${OUTFILE_PATH}"
+su - imagemagick -c "convert ${CONVERSION_PARAMS} ${1} ${OUTFILE_PATH}"
 cat $OUTFILE_PATH > $2
